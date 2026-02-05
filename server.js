3d9d4deff59b3104a28f5b1bced209a01beac851
gladson65 <grauth65@gmail.com>
2026-02-05 16:53:09 +0530

    cors installed and implemented, json parse middleware also implemented

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import env from 'dotenv';
+import cors from 'cors';
 
 // configuring env
 env.config();
@@ -10,3 +11,8 @@ const port = process.env.PORT;
 monthlyServer.listen(port, ()=> {
     console.log(`server is running on the port ${port}`);
 })
+
+// json parse middleware
+monthlyServer.use(express.json());
+// cors middleware
+monthlyServer.use(cors());

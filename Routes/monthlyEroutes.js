d858f69bc57ba0cb49b5ec68b4502c73f64e5c3e
gladson65 <grauth65@gmail.com>
2026-02-06 16:07:31 +0530

    storeExpense route created

--- Routes/monthlyEroutes.js
+++ Routes/monthlyEroutes.js
@@ -1,6 +1,8 @@
 import { register, login } from "../Controllers/users.controller.js";
+import { storeExpense } from "../Controllers/expense.controller.js";
 
 export function monthlyERoutes(monthlyServer) {
     monthlyServer.post("/api/register", register);
     monthlyServer.post("/api/login", login);
+    monthlyServer.post("/api/storeExpense", storeExpense);
 }

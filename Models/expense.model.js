8a05b05181492458633f26c94b56c2f320fbbad2
gladson65 <grauth65@gmail.com>
2026-02-06 15:58:59 +0530

    expense model created

---
+++ Models/expense.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const time = {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+};
+
+const expenseSchema = new Schema({
+    spendOn: {
+        type: Array,
+        required: true
+    },
+    money: {
+        type: Number,
+        required: true
+    }
+}, time)
+
+const expenseModel = mongoose.model("expense", expenseSchema);
+export default expenseModel;
+

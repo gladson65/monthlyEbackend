dc7ffe091d2173b3d014246851f5179bc035fd7b
gladson65 <grauth65@gmail.com>
2026-02-25 14:07:35 +0530

    get expenses by userID has been implemented

--- Controllers/expense.controller.js
+++ Controllers/expense.controller.js
@@ -51,33 +51,46 @@ export function storeExpense(req, res) {
 export function getExpense(req, res) {
 
     const { userID } = req.params;
-    const { email } = req.body;
+    // const { email } = req.body;
 
     // userID key validation
     if (!userID) return res.status(400).json({message: "userID is missing"});
      // email key validation
-    if (!email) return res.status(400).json({message: "email is missing"});
+    // if (!email) return res.status(400).json({message: "email is missing"});
 
     // field validation
-    let isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    if(!isValidEmail) return res.status(400).json({message: "Invalid Email Format"});
+    // let isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    // if(!isValidEmail) return res.status(400).json({message: "Invalid Email Format"});
 
     // check the user is exist or not
-    userModel.findOne({email: email}).then((user)=> {
-        // if user is not registered
-        if (!user) return res.status(400).json({message: "User is not registered"});
+    // userModel.findOne({email: email}).then((user)=> {
+    //     // if user is not registered
+    //     if (!user) return res.status(400).json({message: "User is not registered"});
 
-         // find in expenseModel
-        expenseModel.find({userID: userID}).then((expenses)=> {
-            if(!expenses) {
-                return res.status(404).json({message: "No expense found"});
-            }
+    //     // find in expenseModel
+    //     expenseModel.find({userID: userID}).then((expenses)=> {
+    //         if(!expenses) {
+    //             return res.status(404).json({message: "No expense found"});
+    //         }
 
-            return res.status(200).json({expenses: expenses.length === 0 ? 'No Expense found': expenses});
+    //         return res.status(200).json({expenses: expenses.length === 0 ? 'No Expense found': expenses});
         
-        }).catch((error)=> {
-            return res.status(500).json({error: error.message});
-        })
+    //     }).catch((error)=> {
+    //         return res.status(500).json({error: error.message});
+    //     })
+    // })
+
+    // find in expenseModel
+    expenseModel.find({userID: userID}).then((expenses)=> {
+        // if it is not in the expense Model
+        if(!expenses) {
+            return res.status(404).json({message: "No expense found"});
+        }
+
+        return res.status(200).json({expenses: expenses.length === 0 ? 'No Expense found': expenses});
+    
+    }).catch((error)=> {
+        return res.status(500).json({error: error.message});
     })
 
    
